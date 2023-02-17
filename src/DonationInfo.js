da76cce11bde6f5e4493d6f67d9d2da5b4eecbc6
bhupendrasingh <bhupendra.chauhan@loginradius.com>
2023-02-17 10:14:23 +0530

    Initial commit

---
+++ src/DonationInfo.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from 'react-bootstrap/Button';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+
+export default function DonationInfo() {
+  return (
+    <div className="my-4">
+      <h2> Donation Details</h2>
+      <Form className="my-4">
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Control type="email" placeholder="Enter Your Name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Control type="email" placeholder="Enter Your Name" />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Select>
+            <option value="single">Single</option>
+            <option value="multi">Multi</option>
+            <option value="date">Date</option>
+          </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Control type="email" placeholder="Enter Your Name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Control type="email" placeholder="Enter Your Name" />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Select>
+            <option value="single">Single</option>
+            <option value="multi">Multi</option>
+            <option value="date">Date</option>
+          </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Control type="email" placeholder="Enter Your Name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Control type="email" placeholder="Enter Your Name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Control type="email" placeholder="Enter Your Name" />
+        </Form.Group>
+      <FloatingLabel controlId="floatingTextarea" label="Additional Details" className="mb-3">
+        <Form.Control as="textarea" placeholder="Additional Information" />
+      </FloatingLabel>
+        <Form.Group className="mb-3"></Form.Group>
+        <Button variant="primary" href="/ngodetails" type="submit">
+         Submit
+       </Button>
+      </Form>
+      
+    </div>
+  );
+}
---
+++ src/Home.js
@@ -0,0 +1,32 @@
+import React from "react";
+// import Button from "react-bootstrap/Button";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+
+        <a href="/ngodetails">
+
+          <button className="btn-donate" size="lg">
+            Donate Items
+          </button>
+        </a>
+        {/* <Button className="pickNgo" href="/user" variant="outline-dark" size="lg">
+          Pick up by NGO
+        </Button> */}
+
+        <div>
+          <h1>ACHIEVEMENT</h1>
+        </div>
+
+      </div>
+      <footer>
+      <div id="footer">
+
+<p>Copyright © 2023</p>
+
+</div></footer>
+    </>
+  );
+}
