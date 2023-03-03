c57878239849fbe2bc5f1309b7cb930872838733
bhupendrasingh <bhupendra.chauhan@loginradius.com>
2023-03-03 18:36:02 +0530

    added UI changes

--- src/About.js
+++ src/About.js
@@ -1,46 +1,72 @@
 import React from "react";
 
 export default function About() {
-    return (
-        <>
-            <main>
-                {/*Heading */}
-                <h1 className="title text-md text-center">About us</h1>
-                {/*Heading */}
+  return (
+    <>
+      <main>
+        <div className="container">
+          {/*Heading */}
+          <h1 className="title text-md text-center">About us</h1>
+          {/*Heading */}
 
-                {/*description */}
-                <div className="page-description text-center">
-                    We are a passionate group of individuals committed to creating positive change in the world. Join us as
-                    we work together to make a difference!
-                </div>
-                {/*description */}
+          {/*description */}
+          <div className="page-description text-center">
+            We are a passionate group of individuals committed to creating
+            positive change in the world. Join us as we work together to make a
+            difference!
+          </div>
+          {/*description */}
 
-                {/*profile pictures */}
-                <div className="profile-picture">
-                    <div className="icon">
-                        <a href="https://github.com/nehavyas13"><img  src="./images/neha.png" alt="Neha" /></a>
-                    </div>
-                    <div className="icon">
-                    <a href="https://github.com/krishnaleela567"> <img src="./images/krishna.png" alt="krishna" /></a>
-                    </div>
-                    <div className="icon">
-                    <a href="https://github.com/Bhupendra9414">  <img src="./images/bhupendra.png" alt="Bhupendra" /></a>
-                    </div>
-                    <div className="icon">
-                    <a href="https://github.com/abhir9">  <img src="./images/abhimanyu.png" alt="Abhimanyu" /></a>
-                    </div>
-                    <div className="icon">
-                    <a href="https://github.com/Naman-Agarwal-SE"><img src="./images/naman.png" alt="Naman" /></a>
-                    </div>
-                    <div className="icon">
-                    <a href="https://github.com/Kuldeep-Chhipa"> <img src="./images/kuldeep.png" alt="Kuldeep" /></a>
-                    </div>
-                </div>
-                {/*profile pictures */}
-            </main>
-            <div className="signup text-center">
-                Want to join us as NGO ? <a href="https://forms.gle/Ka1zhB89ABRkQhKD8" target="_blank" rel="noopener noreferrer">Sign up</a>
+          {/*profile pictures */}
+          <div className="profile-picture">
+            <div className="icon">
+              <a href="https://github.com/nehavyas13">
+                <img src="./images/neha.png" alt="Neha" />
+              </a>
             </div>
-        </>
-    )
+            <div className="icon">
+              <a href="https://github.com/krishnaleela567">
+                {" "}
+                <img src="./images/krishna.png" alt="krishna" />
+              </a>
+            </div>
+            <div className="icon">
+              <a href="https://github.com/Bhupendra9414">
+                {" "}
+                <img src="./images/bhupendra.png" alt="Bhupendra" />
+              </a>
+            </div>
+            <div className="icon">
+              <a href="https://github.com/abhir9">
+                {" "}
+                <img src="./images/abhimanyu.png" alt="Abhimanyu" />
+              </a>
+            </div>
+            <div className="icon">
+              <a href="https://github.com/Naman-Agarwal-SE">
+                <img src="./images/naman.png" alt="Naman" />
+              </a>
+            </div>
+            <div className="icon">
+              <a href="https://github.com/Kuldeep-Chhipa">
+                {" "}
+                <img src="./images/kuldeep.png" alt="Kuldeep" />
+              </a>
+            </div>
+          </div>
+          {/*profile pictures */}
+        </div>
+      </main>
+      <div className="signup text-center">
+        Want to join us as NGO ?{" "}
+        <a
+          href="https://forms.gle/Ka1zhB89ABRkQhKD8"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Sign up
+        </a>
+      </div>
+    </>
+  );
 }
--- src/Ngodetails.js
+++ src/Ngodetails.js
@@ -1,92 +1,91 @@
 import React from "react";
 
 export default function Ngodetails(props) {
-    
-    return (
-        <>
-            <main>
-                <div className="container">
-                    <h1 className="title text-md text-center">Nearest NGO list</h1>
-                    <div className="page-description text-center border-b pb-24">
-                        The closest NGOs are listed below based on your filter criteria; you can contact them by phone or
-                        WhatsApp.
+  return (
+    <>
+      <main className="ngodetail-wrap">
+        <div className="container">
+          <h1 className="title text-md text-center">Nearest NGO list</h1>
+          <div className="page-description text-center border-b pb-24">
+            The closest NGOs are listed below based on your filter criteria; you
+            can contact them by phone or WhatsApp.
+          </div>
+          {props.data.length === 0 ? (
+            <p className="text-center">No Result Found</p>
+          ) : (
+            ""
+          )}
+          {props.data.map((item, i) => {
+            var myStringArray = item.contactNumber;
+            // var arrayLength = myStringArray.length;
+            return (
+              <div className="ngo-list " key={i}>
+                <div>
+                  <div className="about-ngo">
+                    <div>
+                      <b>NGO Name :</b>
+                      <p>{item.ngoName}</p>
                     </div>
-                    {props.data.length===0?<p className="text-center">No Result Found</p>:""}
-                    {
-                        props.data.map((item, i) => {
-                            var myStringArray = item.contactNumber;
-                            // var arrayLength = myStringArray.length;
-                            return (
-                                <div className="ngo-list " key={i}>
-                                    <div>
-                                        <div className="about-ngo">
-                                            <div>
-                                                <b>NGO Name :</b>
-                                                <p>{item.ngoName}</p>
-                                            </div>
-                                            <div>
-                                                <b>City :</b>
-                                                <p>{item.city}</p>
-                                            </div>
-                                            <div>
-                                                <b>Type :</b>
-                                                <p>{item.category}</p>
-                                            </div>
-                                        </div>
-                                        <div className="ngo-social-media ">
-                                            <div className="social-links">
-                                                <ul>
-                                                    <li>
-                                                        <a href="/">
-                                                            <img src="./images/map-pin.svg" alt="" />
-                                                        </a>
-                                                    </li>
-                                                    {Object.keys(item.whatsappNumber).length > 0 ?
+                    <div>
+                      <b>City :</b>
+                      <p>{item.city}</p>
+                    </div>
+                    <div>
+                      <b>Type :</b>
+                      <p>{item.category}</p>
+                    </div>
+                  </div>
+                  <div className="ngo-social-media ">
+                    <div className="social-links">
+                      <ul>
+                        <li>
+                          <a href="/">
+                            <img src="./images/map-pin.svg" alt="" />
+                          </a>
+                        </li>
+                        {Object.keys(item.whatsappNumber).length > 0 ? (
+                          <li>
+                            <a
+                              href={`https://wa.me/${item.whatsappNumber}?text=`}
+                            >
+                              <img src="./images/whatsapp.svg" alt="" />
+                            </a>
+                          </li>
+                        ) : null}
 
-                                                        <li>
-                                                            <a href={`https://wa.me/${item.whatsappNumber}?text=`}>
-                                                                <img src="./images/whatsapp.svg" alt="" />
-                                                            </a>
-                                                        </li> : null}
-
-                                                    {myStringArray.map((number, index) => {
-                                                        // console.log(index,number);
-                                                        return (
-                                                            
-                                                            <li key={index}>
-                                                                <a href={`tel:${number}`}>
-                                                                    <img  src="./images/phone-call.svg" alt="" />
-                                                                </a>
-                                                            </li>
-                                                        );
-
-                                                    })}
-                                                </ul>
-                                            </div>
-                                            <div className="like-dislike">
-                                                <ul>
-                                                    <li className="like">
-                                                        <a href="/">
-                                                            <img src="./images/thumbs-up.svg" alt="" />
-                                                        </a>
-                                                    </li>
-                                                    <li className="dislike">
-                                                        <a href="/">
-                                                            <img src="./images/thumbs-down.svg" alt="" />
-                                                        </a>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            )
-                        })
-                    }
-
-
+                        {myStringArray.map((number, index) => {
+                          // console.log(index,number);
+                          return (
+                            <li key={index}>
+                              <a href={`tel:${number}`}>
+                                <img src="./images/phone-call.svg" alt="" />
+                              </a>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </div>
+                    <div className="like-dislike">
+                      <ul>
+                        <li className="like">
+                          <a href="/">
+                            <img src="./images/thumbs-up.svg" alt="" />
+                          </a>
+                        </li>
+                        <li className="dislike">
+                          <a href="/">
+                            <img src="./images/thumbs-down.svg" alt="" />
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
                 </div>
-            </main>
-        </>
-    )
+              </div>
+            );
+          })}
+        </div>
+      </main>
+    </>
+  );
 }
