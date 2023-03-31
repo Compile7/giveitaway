93d34a9ba77381a53f433936e0671c5509713080
bhupendrasingh <bhupendra.chauhan@loginradius.com>
2023-03-31 18:55:10 +0530

    Installable Now

--- src/swDev.js
+++ src/swDev.js
@@ -1,6 +1,5 @@
 export default function swDevfunc() {
   let swUrl = `${process.env.PUBLIC_URL}/sw.js`;
   navigator.serviceWorker.register(swUrl).then((response)=>{
-    // console.warn("response",response)
   });
 }
